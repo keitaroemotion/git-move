95c3f55a262552f645e8ccf00b56845bc95b442d
kei.sugano@veltra.com <kei@veltra.com>
2023-11-28 13:41:48 +0900

    minor update

--- installer.sh
+++ installer.sh
@@ -1,1 +1,3 @@
 cp  git-move /usr/local/bin
+
+# echo ':command! -nargs=0 Bn :call Bn()\nfunction Bn()\n  execute ":! git-move next"\nendfunction\n\n:command! -nargs=0 Bp :call Bp()\nfunction Bp()\n  execute ":! git-move prev"\nendfunction\n' >> ~/.vimrc
